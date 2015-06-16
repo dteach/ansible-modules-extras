5cf8a6dfbcb7847b995b4227e23128986a41eaa1
dteach <davidc.teach@gmail.com>
2015-06-15 22:49:40 -0700

    editing ios_facts

--- network/ios/ios_facts.py
+++ network/ios/ios_facts.py
@@ -39,7 +39,9 @@ class Ios(object):
             acct.set_authorization_password(enable)
 
         self.q.add_account(acct)
-        self.q.run(self.hosts,self.get_ver)
+        print self.hosts
+
+        print self.q.run(self.hosts,self.get_ver)
         self.q.destroy()
 
     def get_ver(self, job, host, conn):
