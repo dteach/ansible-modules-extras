d6d55a81b99a6e91e8668e7f5e63199014bed2a8
dteach <davidc.teach@gmail.com>
2015-06-15 22:14:03 -0700

    editing ios_facts

--- network/ios/ios_facts.py
+++ network/ios/ios_facts.py
@@ -58,6 +58,7 @@ def main():
     queue = Queue(**{'verbose':0})
     queue.add_account(acct)
     task = queue.run(host,do_work)
+    print task
     while not task.is_completed:
         pass
     queue.destroy()
