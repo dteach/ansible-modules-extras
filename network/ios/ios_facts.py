6a4c3455d13158a6fe4d59f4e97fb3fe29066d1e
dteach <davidc.teach@gmail.com>
2015-06-16 23:53:38 -0700

    editing ios_facts

--- network/ios/ios_facts.py
+++ network/ios/ios_facts.py
@@ -43,6 +43,7 @@ class dev_q(object):
             tmp_host.set_protocol(default_protocol)
             tmp_host.set_option('driver', default_driver)
             self.hosts.append(tmp_host)
+            print self.hosts
 
     def add_accounts(self, name, password, enable=False):
         acct = Account(name=name, password=password)
